9d0fe812a18dd1194f83f3cf18947b17e49bdcb0
Lucas Andrade <lucas.me@yahoo.com>
2021-07-28 18:00:39 +0100

    adding login route and view

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ use App\Http\Controllers;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/login', function () {
+    return view('login');
+});
 Route::post('/login', [Controllers\AuthController::class, 'login'])->name('login');
 Route::get('/logout', [Controllers\AuthController::class, 'logout'])->name('logout');
 Route::middleware(['auth'])->group(function () {
