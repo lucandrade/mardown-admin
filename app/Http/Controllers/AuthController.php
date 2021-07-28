2dd2630dd6c41be53080f08188304a05095d0c7d
Lucas Andrade <lucas.me@yahoo.com>
2021-07-28 17:24:53 +0100

    adding auth controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+final class AuthController extends Controller
+{
+    public function login(Request $request): Response
+    {
+        $authenticated = Auth::attempt([
+            'username' => $request->input('username'),
+            'password' => $request->input('password'),
+        ]);
+
+        if (!$authenticated) {
+            return redirect('/login')->with('error', 'Invalid credentials');
+        }
+
+        return redirect('/admin');
+    }
+}
