65b988c36a2b89efdeb8620de0f550cef84f3294
Lucas Andrade <lucas.me@yahoo.com>
2021-08-02 18:56:36 +0100

    installing frontend dependencies

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -12,11 +12,11 @@
     <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
     <div class="form-floating">
-      <input type="text" class="form-control">
+      <input type="text" class="form-control" name="username">
       <label for="floatingInput">Username</label>
     </div>
     <div class="form-floating">
-      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+      <input type="password" class="form-control" id="floatingPassword" placeholder="Password" name="password">
       <label for="floatingPassword">Password</label>
     </div>
     <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
