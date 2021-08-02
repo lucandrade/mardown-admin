7d5740214e3b75a034a089d4284bd65f5f1b85a1
Lucas Andrade <lucas.me@yahoo.com>
2021-08-02 21:06:29 +0100

    adjusting admin topbar

--- resources/views/components/admin-header.blade.php
+++ resources/views/components/admin-header.blade.php
@@ -1,5 +1,23 @@
-<nav class="navbar navbar-light bg-light">
+<nav class="navbar navbar-expand navbar-light bg-light">
   <div class="container-fluid">
     <span class="navbar-brand mb-0 h1">Admin Markdown</span>
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a
+          class="nav-link {{ url()->current() === url('/admin') ? 'active' : '' }}"
+          aria-current="page"
+          href="/admin">
+          Posts
+        </a>
+      </li>
+      <li class="nav-item">
+        <a
+          class="nav-link {{ url()->current() === url('/admin/create') ? 'active' : '' }}"
+          aria-current="page"
+          href="/admin/create">
+          New post
+        </a>
+      </li>
+    </ul>
   </div>
 </nav>
