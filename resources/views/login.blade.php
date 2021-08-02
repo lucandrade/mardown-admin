40576e22071b1505b3399488b7771c107a194ddb
Lucas Andrade <lucas.me@yahoo.com>
2021-08-02 21:31:01 +0100

    adjusting login view

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,13 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Laravel</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-</head>
-<body>
-  <form method="post" action="/login">
+@extends('layouts.default')
+
+@section('content')
+  <form method="post" class="mt-4">
     <input type="hidden" name="_token" value="{{ csrf_token() }}" />
     <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
@@ -22,5 +16,4 @@
     <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
     <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
   </form>
-</body>
-</html>
+@endsection
